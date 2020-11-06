61b5c801904c192103eac2bf1102bbe92bd9e1e9
Sleiven <73979961+Sleiven@users.noreply.github.com>
2020-11-05 20:18:08 -0500

    Add files via upload
    Código que ejecuta la función PIE.

---
+++ pie.R
@@ -0,0 +1,25 @@
+PIE <- function(ni){ 
+  # version 2.0| Oct 2020 | Autores: Ramirez Paola, David Camila, Murillo David
+  
+  # Descripcion:
+  # Se mide la diversidad de especies de acuerdo a
+  # el indice PIE a partir de un vector de 
+  # abundancias por especie
+  
+  # Argumentos:
+  # ni <- vector numerico con las abundancias de especies
+  
+  # Codigo:
+  n <- sum(ni)
+  k <- length(ni)
+  sqpi <- (ni/n) ^ 2 
+  suma<- sum(sqpi[1:k])
+  (n/(n -1)) * (1 - suma)
+  
+  # Valor:
+  # El resultado del calculo del indice PIE
+  
+  # Ejemplo de uso:
+  # PIE(ni = c(11,62,14,92,11,10,4))
+  # PIE(c(11,77,21,158,10,19,6))
+}
