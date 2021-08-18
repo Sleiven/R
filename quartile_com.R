8e6a1fc27bdc30a7226cc48dcf27bdc34879da7a
Sleiven <73979961+Sleiven@users.noreply.github.com>
2021-08-18 16:31:58 -0500

    Update quartile_com.R

--- quartile_com.R
+++ quartile_com.R
@@ -1,5 +1,5 @@
 quartile_com <- function(datos){
-  # version 1.0 | Mar 2021 | Autor: Murillo David
+  # Autor: Murillo-Cardona David | Mar 2021 | version 1.0
   
   # Descripcion:
     # Modificacion realizada sobre la funcion summary {stats}
